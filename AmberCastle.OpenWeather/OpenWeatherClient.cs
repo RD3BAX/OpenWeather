676e5258d9c0b59c73f93916889d3ce31b369e21
Magistr <MagistrTNT@msn.com>
2021-06-07 12:12:40 +0300

    Дополнительные библиотеки для работы с HttpClient

---
+++ AmberCastle.OpenWeather/OpenWeatherClient.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+
+namespace AmberCastle.OpenWeather
+{
+    class OpenWeatherClient
+    {
+        #region Поля
+
+        private readonly HttpClient _client;
+
+        #endregion // Поля
+
+        #region Конструктор
+
+        public OpenWeatherClient(HttpClient Client)
+        {
+            _client = Client;
+        }
+
+        #endregion // Конструктор
+    }
+}
