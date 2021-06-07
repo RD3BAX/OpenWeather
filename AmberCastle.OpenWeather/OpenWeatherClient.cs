7980c4a875ca6e621da29adefa97a2479d009a57
Magistr <MagistrTNT@msn.com>
2021-06-07 23:29:50 +0300

    "Сырая" реализация методов получения координат

--- AmberCastle.OpenWeather/OpenWeatherClient.cs
+++ AmberCastle.OpenWeather/OpenWeatherClient.cs
@@ -1,22 +1,88 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace AmberCastle.OpenWeather
 {
-    class OpenWeatherClient
+    public class OpenWeatherClient
     {
         #region Поля
 
         private readonly HttpClient _client;
+        private readonly IConfiguration _config;
+        private readonly string _ApiKey;
 
         #endregion // Поля
 
+        #region Свойства
+
+        #endregion // Свойства
+
+        #region Методы
+
+        #region Geo 1.0
+
+        public async Task<WeatherLocation[]> GetDirectGeocoding(string Name, int Limit = 0)
+        {
+            return await _client.GetFromJsonAsync<WeatherLocation[]>(
+                $"/geo/1.0/direct?q={Name}" +
+                (Limit == 0 ? "" : $"&limit={Limit}") +
+                $"&appid={_ApiKey}");
+        } 
+        
+        public async Task<WeatherLocation[]> GetReverseGeocoding(double Lat, double Lon,int Limit = 0)
+        {
+            return await _client.GetFromJsonAsync<WeatherLocation[]>(
+                $"/geo/1.0/reverse?lat={Lat}&lon={Lon}" +
+                (Limit == 0 ? "" : $"&limit={Limit}") +
+                $"&appid={_ApiKey}");
+        }
+
+        public async Task<WeatherZipLocation> GetCoordinatesByZip(string ZipCode)
+        {
+            return await _client.GetFromJsonAsync<WeatherZipLocation>(
+                $"/geo/1.0/zip?zip={ZipCode}" +
+                $"&appid={_ApiKey}");
+        }
+
+        #endregion // Geo 1.0
+
+        #endregion // Методы
+
         #region Конструктор
 
-        public OpenWeatherClient(HttpClient Client)
+        public OpenWeatherClient(HttpClient Client, IConfiguration config)
         {
             _client = Client;
+            _config = config;
+            _ApiKey = config["OpenWeather:ApiKey"];
         }
 
         #endregion // Конструктор
     }
+
+
+    public class WeatherLocation
+    {
+        public string name { get; set; }
+        public Dictionary<string, string> local_names { get; set; }
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public string country { get; set; }
+        public string state { get; set; }
+    }
+
+
+    public class WeatherZipLocation
+    {
+        public string zip { get; set; }
+        public string name { get; set; }
+        public float lat { get; set; }
+        public float lon { get; set; }
+        public string country { get; set; }
+    }
+
+    
 }
--- Test/AmberCastle.OpenWeather.TestConsole/Program.cs
+++ Test/AmberCastle.OpenWeather.TestConsole/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace AmberCastle.OpenWeather.TestConsole
 {
@@ -19,14 +24,45 @@ namespace AmberCastle.OpenWeather.TestConsole
 
         private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
-            
+            services.AddHttpClient<OpenWeatherClient>(client =>
+                client.BaseAddress = new Uri(host.Configuration["OpenWeather:Http"]))
+                .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                .AddPolicyHandler(GetRetryPolicy())
+                ;
+            //Services.GetRequiredService<OpenWeatherClient>().ApiKey = host.Configuration["OpenWeather:ApiKey"];
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            var jitter = new Random();
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .WaitAndRetryAsync(6, retry_attempt =>
+                    TimeSpan.FromSeconds(Math.Pow(2, retry_attempt)) +
+                    TimeSpan.FromMilliseconds(jitter.Next(0, 1000)));
         }
 
         static async Task Main(string[] args)
         {
             using var host = Hosting;
             await host.StartAsync();
-            
+
+            var weather = Services.GetRequiredService<OpenWeatherClient>();
+
+            var location = await weather.GetDirectGeocoding("Moscow",10);
+
+
+            var key = "ru";
+            string name;
+            if (location[0].local_names.ContainsKey(key))
+            {
+                name = location[0].local_names[key];
+            }
+
+            var reverse = await weather.GetReverseGeocoding(location[0].lat, location[0].lon);
+
+            var zipLocal = await weather.GetCoordinatesByZip("26651");
+
             Console.WriteLine("Завершено!");
             Console.ReadLine();
             await host.StopAsync();
