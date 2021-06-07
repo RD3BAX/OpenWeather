4363e34981d04dc8f1510c33fd908c343cce812c
Magistr <MagistrTNT@msn.com>
2021-06-07 10:12:49 +0300

    Добавьте файлы проекта.

---
+++ Test/AmberCastle.OpenWeather.TestConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AmberCastle.OpenWeather.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
